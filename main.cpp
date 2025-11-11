2daebbee228fff0f3fc5fbcd212a410bdbdc070a
Lucas Ângelo <lucasangeloss27@gmail.com>
2025-11-10 22:44:49 -0300

    Função deletar
    Corrected spelling errors, improved output formatting, and added a delete movie function in the cinema management system.

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string>
 using namespace std;
 
-/*Criando os bancos de dados*/
+/*Criandos os bancos de dados*/
 struct Filme {
 	int id;
 	string titulo;
@@ -16,6 +16,7 @@ struct Sala {
 	int idFilme;
 	string tituloFilme;
 	float precoIngresso;
+	/*vector<vector<Assento>> assentos;*/
 };
 
 struct Pagamento {
@@ -23,11 +24,10 @@ struct Pagamento {
 	int idSala;
 	float preco_ingresso;
 	int quantidade_ingresso;
-
-	float precoFinal() const {
+	/*função para calcular o valor final*/
+	float precoFinal() const{
 		return preco_ingresso * quantidade_ingresso;
 	}
-
 	bool debito = true;
 };
 
@@ -38,74 +38,95 @@ vector<Filme> filmes;
 /*FUNÇÕES DOS FILMES*/
 void adicionarFilme() {
 	Filme f;
-	cout << "\nDigite o titulo do filme: ";
-	cin.ignore();
+	cout << "\nTitulo: ";
 	getline(cin, f.titulo);
-
 	cout << "\nAvaliacao (0 a 10): ";
 	cin >> f.avaliacao;
-
-	if (f.avaliacao < 0) f.avaliacao = 0;
-	if (f.avaliacao > 10) f.avaliacao = 10;
-
+	if (f.avaliacao < 0){
+		f.avaliacao = 0;
+	} 
+	if (f.avaliacao > 10) {
+		f.avaliacao = 10;
+	}
 	char resposta;
 	do {
 		cout << "\nO filme esta dublado? (S/N): ";
 		cin >> resposta;
-
 		if (resposta == 'S' || resposta == 's') {
 			f.dublado = true;
 		} else if (resposta == 'N' || resposta == 'n') {
 			f.dublado = false;
 		} else {
-			cout << "\n[ERRO] Opcao invalida! Digite apenas S ou N.\n";
+			cout << "Opcao invalida! Digite apenas S ou N." << endl;
 		}
 	} while (resposta != 'S' && resposta != 's' && resposta != 'N' && resposta != 'n');
-
+	
 	f.id = filmes.size() + 1;
 	filmes.push_back(f);
-
-	cout << "\nFilme adicionado com sucesso!\n";
-	cout << "-----------------------------\n";
+	cout << "Filme adicionado com sucesso!" << endl;
+	cout << "--------------------" << endl;
 }
 
 void listarFilme() {
 	if (filmes.empty()) {
-		cout << "\nNenhum filme cadastrado.\n";
+		cout << "Nenhum filme cadastrado." << endl;
 		return;
 	}
-
 	for (auto &f : filmes) {
-		cout << "\n----------------------\n";
+		cout << "----------------------" << endl;
 		cout << "ID: " << f.id << endl;
-		cout << "Titulo: " << f.titulo << endl;
-		cout << "Avaliacao: " << f.avaliacao << endl;
-		cout << "Dublado: " << (f.dublado ? "Sim" : "Nao") << endl;
+        cout << "Titulo: " << f.titulo << endl;
+        cout << "Avaliacao: " << f.avaliacao << endl;
+        cout << "Dublado: " << (f.dublado ? "Sim" : "Nao") << endl;
+	}
+}
+
+void deletarFilme() {
+	if (filmes.empty()) {
+		cout << "Nenhum filme foi criado." << endl;
+		return;
+	}
+	for (auto &f : filmes) {
+		cout << "\nID: " << f.id << " - " << f.titulo << endl;
+	}
+	int idEscolhido;
+	cout << "Digite o ID do filme em que deseja deletar: ";
+	cin >> idEscolhido;
+	bool encontrado = true;
+	for (auto it = filmes.begin(); it != filmes.end(); it++) {
+		if(it->id == idEscolhido) {
+			cout << "\nFilme " << it->titulo << " deletado com sucesso!" << endl;
+			cout << "\n--------------------------\n";
+			filmes.erase(it);
+			encontrado = true;
+			break;
+		}
+	}
+	if (!encontrado) {
+		cout << "[ERROR] ID inexistente." << endl;
 	}
 }
 
 /*FUNÇÕES DA SALA*/
 void criarSala() {
 	if (filmes.empty()) {
-		cout << "\nNao eh possivel criar uma sala. Cadastre um filme primeiro.\n";
+		cout << "Nao eh possivel criar uma sala. Cadastre um filme primeiro.";
 		return;
 	}
-
 	Sala s;
 	s.id = salas.size() + 1;
-
-	cout << "\n------------------------\n";
-	cout << "Filmes disponiveis:\n";
+	cout << "------------------------" << endl;
+	cout << "Filmes disponiveis: " << endl;
 	for (auto &f : filmes) {
-		cout << "ID: " << f.id << " - " << f.titulo << endl;
+		cout << "ID :" << f.id << " - " << f.titulo << endl;
 	}
-
+	
 	int idEscolhido;
-	cout << "\nDigite o ID do filme que sera adicionado nessa sala: ";
+	cout << "Digite o ID do filme que sera adicionado nessa sala: ";
 	cin >> idEscolhido;
-
-	bool encontrado = false;
-	for (auto &f : filmes) {
+	
+	bool encontrado = true;
+	for(auto &f : filmes) {
 		if (f.id == idEscolhido) {
 			s.idFilme = f.id;
 			s.tituloFilme = f.titulo;
@@ -113,49 +134,44 @@ void criarSala() {
 			break;
 		}
 	}
-
 	if (!encontrado) {
-		cout << "\n[ERRO] O filme nao foi encontrado. Sala nao criada.\n";
+		cout << "O filme não foi encontrado." << endl;
+		cout << "Sala nao criada.";
 		return;
 	}
-
-	cout << "\nPreco do ingresso: ";
+	
+	cout << "Preco do ingresso: ";
 	cin >> s.precoIngresso;
-
 	salas.push_back(s);
-	cout << "\nSala criada com sucesso!\n";
+	cout << "Sala criada com sucesso!";
 }
 
 void listarSalas() {
-	if (salas.empty()) {
-		cout << "\nNenhuma sala foi criada ainda.\n";
+	if(salas.empty()) {
+		cout << "Nenhuma sala foi criada ainda.";
 		return;
 	}
-
 	for (auto &s : salas) {
-		cout << "\n--------------------------\n";
+		cout << "--------------------------";
 		cout << "ID da sala: " << s.id << endl;
 		cout << "Titulo do filme: " << s.tituloFilme << endl;
-		cout << "Preco do ingresso: R$" << s.precoIngresso << endl;
+		cout << "Preco do ingresso: " << s.precoIngresso << endl;
 	}
 }
 
 /*FUNÇÕES DE PAGAMENTO*/
 void pagamento() {
 	Pagamento p;
-	if (salas.empty()) {
-		cout << "\nNao ha salas disponiveis no momento. Tente novamente mais tarde.\n";
+	if(salas.empty()) {
+		cout << "Nao ha salas disponiveis no momento. Tente novamente mais tarde." << endl;
 		return;
 	}
-
-	cout << "\n----------------------\n";
-	cout << "-- SALAS DISPONIVEIS --\n";
+	cout << "----------------------" << endl;
+	cout << "-- SALAS DISPONIVEIS --";
 	listarSalas();
-
 	int idEscolhido;
-	cout << "\nDigite o ID da sala em que voce ira comprar: ";
+	cout << "Digite o ID da sala em que voce ira pegar: ";
 	cin >> idEscolhido;
-
 	bool encontrado = false;
 	for (auto &s : salas) {
 		if (s.id == idEscolhido) {
@@ -165,88 +181,88 @@ void pagamento() {
 			break;
 		}
 	}
-
 	if (!encontrado) {
-		cout << "\n[ERRO] Sala nao encontrada.\n";
+		cout << "Sala nao encontrada." << endl;
 		return;
 	}
-
+	
 	p.codigo = pagamentos.size() + 1;
-
-	cout << "\nQuantidade de ingressos: ";
+	cout << "Quantidade de ingressos: ";
 	cin >> p.quantidade_ingresso;
-
+	
 	float total = p.precoFinal();
-	cout << "\nPreco total: R$" << total << endl;
-
+	cout << "Preco total: " << total << endl;
+	
 	char forma_pagamento;
+	cout << "Pagamento sera no debito (D) ou credito (C)?\n";
+	cin >> forma_pagamento;
 	do {
-		cout << "\nPagamento sera no debito (D) ou credito (C)? ";
-		cin >> forma_pagamento;
-
-		if (forma_pagamento == 'D' || forma_pagamento == 'd') {
-			p.debito = true;
-		} else if (forma_pagamento == 'C' || forma_pagamento == 'c') {
-			p.debito = false;
-		} else {
-			cout << "\n[ERRO] Valor invalido! Digite apenas D ou C.\n";
-		}
-	} while (forma_pagamento != 'D' && forma_pagamento != 'd' && forma_pagamento != 'C' && forma_pagamento != 'c');
-
+    	if (forma_pagamento == 'D' || forma_pagamento == 'd') {
+    		p.debito = true;
+    	} else if (forma_pagamento == 'C' || forma_pagamento == 'c'){
+    		p.debito = false;
+    	} else {
+    	    cout << "[ERRO] Voce digitou um valor invalido" << endl;
+    	    cout << "Digite apenas D ou C" << endl;
+    	} 
+	} while (forma_pagamento == 'D' || forma_pagamento == 'd' || forma_pagamento == 'C' || forma_pagamento == 'c');
 	pagamentos.push_back(p);
-
-	cout << "\nPagamento registrado com sucesso!\n";
+	
+	cout << "Pagamento registrado com sucesso!" << endl;
 	cout << "Codigo: " << p.codigo << endl;
-	cout << "Sala ID: " << p.idSala << endl;
-	cout << "Total pago: R$" << total << endl;
-	cout << "Forma de pagamento: " << (p.debito ? "Debito" : "Credito") << endl;
+    cout << "Sala ID: " << p.idSala << endl;
+    cout << "Total pago: R$" << total << endl;
+    cout << "Forma de pagamento: " << (p.debito ? "Debito" : "Credito") << endl;
 }
 
-void folhaPagamento() {
-	if (pagamentos.empty()) {
-		cout << "\nNenhum pagamento efetuado.\n";
+void folhaPagamento(){
+	if(pagamentos.empty()) {
+		cout << "Nenhum pagamento efetuado." << endl;
 		return;
 	}
-
+	
 	float total_arrecadado = 0;
-	for (auto &p : pagamentos) {
-		cout << "\nCodigo do pagamento: " << p.codigo << endl;
+	for(auto &p : pagamentos) {
+		cout << "Codigo do pagamento: " << p.codigo << endl;
 		cout << "ID da sala: " << p.idSala << endl;
 		cout << "Quantidade de ingressos: " << p.quantidade_ingresso << endl;
-		cout << "Preco final: R$" << p.precoFinal() << endl;
+		cout << "Preco final: " << p.precoFinal() << endl;
 		cout << "Forma de pagamento: " << (p.debito ? "Debito" : "Credito") << endl;
-		cout << "------------------------------------\n";
-
+		cout << "------------------------------------" << endl;
+		
 		total_arrecadado += p.precoFinal();
 	}
-
-	cout << "\nTOTAL ARRECADADO: R$" << total_arrecadado << endl;
+	
+	cout << "TOTAL ARRECADADO: R$" << total_arrecadado << endl;
 }
 
 /*Parte administrativa*/
-void administrador() {
+void administrador(){
 	int opc;
 	do {
-		cout << "\n--- CINEMA ALEGRIA ---\n";
-		cout << "--  Menu do Administrador  --\n";
-		cout << "1 - Adicionar filme\n";
-		cout << "2 - Listar filmes\n";
-		cout << "3 - Adicionar sala\n";
-		cout << "4 - Listar salas\n";
-		cout << "5 - Folha de Pagamento\n";
-		cout << "0 - Voltar\n";
-		cout << "\nEscolha uma das opcoes: ";
-		cin >> opc;
-		cin.ignore();
-
-		switch (opc) {
+	
+	cout << "--- CINEMA ALEGRIA ---" << endl;
+	cout << "--  Todas opcoes  --" << endl;
+	cout << "1- Adicionar filme" << endl;
+	cout << "2- Listar filmes" << endl;
+	cout << "3- Adicionar sala" << endl;
+	cout << "4- Listar salas" << endl;
+	cout << "5- Deletar filme" << endl;
+	cout << "6- Folha de Pagamento" << endl;
+	cout << "0- Voltar" << endl;
+	cout << "-- Escolha uma das opcoes --" << endl;
+	cin >> opc;
+	cin.ignore();
+	
+		switch(opc) {
 			case 1: adicionarFilme(); break;
 			case 2: listarFilme(); break;
 			case 3: criarSala(); break;
-			case 4: listarSalas(); break;
-			case 5: folhaPagamento(); break;
-			case 0: cout << "\nVoltando ao menu principal...\n"; break;
-			default: cout << "\n[ERRO] Valor invalido!\n"; break;
+			case 4: listarSalas(); break; 
+			case 5: deletarFilme(); break;
+			case 6: folhaPagamento(); break;
+			case 0: cout << "Voltando ao menu principal..." << endl; break;
+			default: cout << "[ERROR] Valor invalido!" << endl; break;
 		}
 	} while (opc != 0);
 }
@@ -255,42 +271,41 @@ void administrador() {
 void cliente() {
 	int opc;
 	do {
-		cout << "\n--- CINEMA ALEGRIA ---\n";
-		cout << "--  Menu do Cliente  --\n";
-		cout << "1 - Salas e Filmes disponiveis\n";
-		cout << "2 - Pagar ingresso\n";
-		cout << "0 - Voltar\n";
-		cout << "\nEscolha uma das opcoes: ";
-		cin >> opc;
-		cin.ignore();
-
-		switch (opc) {
+	
+	cout << "--- CINEMA ALEGRIA ---";
+	cout << "--  Todas opcoes  --" << endl;
+	cout << "1- Salas e Filmes disponiveis" << endl;
+	cout << "2- Pagar ingresso" << endl;
+	cout << "0- Voltar" << endl;
+	cout << "-- Escolha uma das opcoes --" << endl;
+	cin >> opc;
+	cin.ignore();
+	
+		switch(opc) {
 			case 1: listarSalas(); break;
 			case 2: pagamento(); break;
-			case 0: cout << "\nVoltando ao menu principal...\n"; break;
-			default: cout << "\n[ERRO] Valor invalido!\n"; break;
+			case 0: cout << "Voltando ao menu principal..." << endl; break;
+			default: cout << "[ERROR] Valor invalido!" << endl; break;
 		}
 	} while (opc != 0);
 }
 
-int main() {
+int main(){
 	int opc;
 	do {
-		cout << "\n======= CINEMA ALEGRIA =======\n";
-		cout << "1 - Administrador\n";
-		cout << "2 - Cliente\n";
-		cout << "0 - Finalizar\n";
-		cout << "\nEscolha uma das opcoes: ";
-		cin >> opc;
-		cin.ignore();
-
+		
+	cout << "Escolha uma das opcoes: " << endl;
+	cout << "1- Administrador" << endl;
+	cout << "2- Cliente" << endl;
+	cout << "0- Finalizar" << endl;
+	cin >> opc;
+	cin.ignore();	
 		switch (opc) {
 			case 1: administrador(); break;
 			case 2: cliente(); break;
-			case 0: cout << "\nFinalizando...\n"; break;
-			default: cout << "\n[ERRO] Valor invalido!\n"; break;
+			case 0: cout << "Finalizando..." << endl; break;
+			default: cout << "[ERROR] Valor invalido!";
 		}
 	} while (opc != 0);
-
 	return 0;
 }
